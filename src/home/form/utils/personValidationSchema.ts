7bc6e079b3e791d6c6e41b60d9c2fa42c1462b6f
Øyvind Julsrud <oyvind.julsrud@rs.no>
2024-05-07 09:59:05 +0200

    fix: Add type to you

--- src/home/form/utils/personValidationSchema.ts
+++ src/home/form/utils/personValidationSchema.ts
@@ -1,6 +1,7 @@
 import * as Yup from "yup";
+import { Person } from "../../../utils/types";
 
-export const personValidationSchema = Yup.object({
+export const personValidationSchema = Yup.object<Person>({
   fullName: Yup.string().required("Fullt navn er påkrevd"),
   email: Yup.string()
     .email("Ugyldig e-postadresse")
